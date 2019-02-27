0e63ce5bdffa1270e35e33bb06fd58b8b043d3f8
Michelle Austin <michelletoelle@gmail.com>
2019-02-27 11:57:46 +0000

    Added back FontAwesome files for social icons, tweaking colours.

--- src/App.js
+++ src/App.js
@@ -43,15 +43,13 @@ class App extends Component {
     this.getResumeData();
   }
 
+
   render() {
     return (
       <div className="App">
         <Header data={this.state.resumeData.main}/>
         <About data={this.state.resumeData.main}/>
         <Resume data={this.state.resumeData.resume}/>
-        <Portfolio data={this.state.resumeData.portfolio}/>
-        <Testimonials data={this.state.resumeData.testimonials}/>
-        <Contact data={this.state.resumeData.main}/>
         <Footer data={this.state.resumeData.main}/>
       </div>
     );
